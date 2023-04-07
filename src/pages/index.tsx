4bb882ca41591053626183d17deb29bc433b5f12
Ne0MM <pablomaximiliano260@gmail.com>
2023-04-07 13:36:07 -0300

    colabLogo gtg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
-import ColabLogo from './Components/ColabLogo'
-
 export default function Home() {
   return (
     <>
-      <div className='flex flex-col justify-center items-center h-screen bg-white'>
-        <ColabLogo grow={true} text={true}/>
-      </div>
     </>
   )
 }
